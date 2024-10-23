a8207b9c48425a20bfab615899e1dc52d1a927b7
Lucca Aufieri <aufieriz@gmail.com>
2024-10-23 01:13:48 -0300

    Modificado o DB Context

--- Infraestrutura/DB/DBContext.cs
+++ Infraestrutura/DB/DBContext.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace minmal_api.Infraestrutura.DB
 {
-    public class DBContext
+    public class DBContext : DbContext
     {
-        
+        public DBContext (DbContextOptions<DbContext> options) : base(options){
+            
+        }
     }
 }
